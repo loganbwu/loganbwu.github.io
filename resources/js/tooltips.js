6932c01fe281a86d84249214ae2d5d2cfc4780ae
Logan Wu <logan.wu@dhhs.vic.gov.au>
2021-09-27 17:16:47 +1000

    switch to boostrap for tooltips

---
+++ resources/js/tooltips.js
@@ -0,0 +1,4 @@
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[title]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
