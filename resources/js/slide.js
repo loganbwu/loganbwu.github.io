d0beb1be4a4c4b318ba65e96b7cc6fba0a4805ca
Logan Wu <loganbwu@gmail.com>
2021-10-13 12:45:39 +1100

    Fix swipe anim

--- resources/js/slide.js
+++ resources/js/slide.js
@@ -1,26 +1,26 @@
 (function($) {
-
-  /**
-   * Copyright 2012, Digital Fusion
-   * Licensed under the MIT license.
-   * http://teamdf.com/jquery-plugins/license/
-   *
-   * @author Sam Sehnert
-   * @desc A small plugin that checks whether elements are within
-   *     the user visible viewport of a web browser.
-   *     only accounts for vertical position, not horizontal.
-   */
-
-  $.fn.visible = function() {
-    
-      var $t            = $(this),
-          $w            = $(window),
-          viewBottom    = $w.scrollTop() + $w.height(),
-          elTop         = $t.offset().top
-    
-  return elTop <= viewBottom;
-
-  };
+	
+	/**
+		* Copyright 2012, Digital Fusion
+		* Licensed under the MIT license.
+		* http://teamdf.com/jquery-plugins/license/
+		*
+		* @author Sam Sehnert
+		* @desc A small plugin that checks whether elements are within
+		*     the user visible viewport of a web browser.
+		*     only accounts for vertical position, not horizontal.
+	*/
+	
+	$.fn.visible = function() {
+		
+		var $t            = $(this),
+		$w            = $(window),
+		viewBottom    = $w.scrollTop() + $w.height(),
+		elTop         = $t.offset().top
+		
+		return elTop <= viewBottom;
+		
+	};
     
 })($);
 
@@ -31,36 +31,37 @@ var allMods = $(".section");
 var allH1 = $("h1");
 
 allMods.each(function(i, el) {
-  var el = $(el);
-  if (el.visible()) { 
-    el.addClass("already-visible"); 
-  } else {
-    el.addClass("come-in-init");
-  }
+	var el = $(el);
+	if (el.visible()) { 
+		el.addClass("already-visible"); 
+	}
 });
 
 allH1.each(function(i, el) {
-  var el = $(el);
-  if (el.visible()) {
-    el.addClass("reveal"); 
-    el.addClass("already-visible"); 
-  } 
+	var el = $(el); 
+	if (el.visible()) {
+		el.addClass("reveal"); 
+		el.addClass("already-visible");
+	}
+	else {
+		el.addClass("reveal-init");
+	}
 });
 
 win.scroll(function(event) {
-  
-  allMods.each(function(i, el) {
-    var el = $(el);
-    if (el.visible()) {
-      el.addClass("come-in"); 
-    } 
-  });
-  
-  allH1.each(function(i, el) {
-    var el = $(el);
-    if (el.visible()) {
-      el.addClass("reveal"); 
-    } 
-  });
-  
+	
+	allMods.each(function(i, el) {
+var el = $(el);
+if (el.visible()) {
+el.addClass("come-in"); 
+	} 
+	});
+	
+	allH1.each(function(i, el) {
+		var el = $(el);
+		if (el.visible()) {
+			el.addClass("reveal"); 
+		} 
+	});
+	
 });
