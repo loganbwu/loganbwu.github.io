d8ea38e9f8986c2971b8c5897e504bf2b059a2dd
Logan Wu <loganbwu@gmail.com>
2021-10-15 09:36:18 +1100

    Switch to use section tags

--- resources/js/slide.js
+++ resources/js/slide.js
@@ -1,32 +1,17 @@
 (function($) {
-	
-	/**
-		* Copyright 2012, Digital Fusion
-		* Licensed under the MIT license.
-		* http://teamdf.com/jquery-plugins/license/
-		*
-		* @author Sam Sehnert
-		* @desc A small plugin that checks whether elements are within
-		*     the user visible viewport of a web browser.
-		*     only accounts for vertical position, not horizontal.
-	*/
-	
 	$.fn.visible = function() {
-		
 		var $t            = $(this),
 		$w            = $(window),
 		viewBottom    = $w.scrollTop() + $w.height(),
 		elTop         = $t.offset().top
 		
 		return elTop <= viewBottom;
-		
 	};
-    
 })($);
 
 var win = $(window);
 
-var allMods = $(".section");
+var allMods = $("section");
 
 var allH1 = $("h1");
 
@@ -51,10 +36,10 @@ allH1.each(function(i, el) {
 win.scroll(function(event) {
 	
 	allMods.each(function(i, el) {
-var el = $(el);
-if (el.visible()) {
-el.addClass("come-in"); 
-	} 
+		var el = $(el);
+		if (el.visible()) {
+			el.addClass("come-in"); 
+		} 
 	});
 	
 	allH1.each(function(i, el) {
@@ -64,4 +49,4 @@ el.addClass("come-in");
 		} 
 	});
 	
-});
+});	
