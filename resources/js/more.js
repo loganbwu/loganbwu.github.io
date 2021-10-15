f15a8cf0f6bd9be9869a0840b90d1a6ae9c9e4ad
Logan Wu <loganbwu@gmail.com>
2021-10-15 14:31:44 +1100

    Dynamic more text

--- resources/js/more.js
+++ resources/js/more.js
@@ -1,12 +1,12 @@
 let getSiblings = function (e) {
     // for collecting siblings
-    let siblings = []; 
-    // first child of the parent node
-	let sibling = e.previousSibling;
+	console.log(e)
+    let siblings = [];
     
     // collecting siblings
+	let sibling = e.previousSibling;
     while (sibling) {
-        if (sibling.nodeType === 1 && sibling !== e) {
+        if (sibling.nodeType === 1 && sibling.classList.contains("event-more")) {
             siblings.push(sibling);
         }
         sibling = sibling.previousSibling;
@@ -17,13 +17,24 @@ let getSiblings = function (e) {
 
 var morebuttons = document.getElementsByClassName("show-more");
 for (const mb of morebuttons) {
+	// set text dynamically
+	let n_siblings = getSiblings(mb).length;
+	mb.innerHTML = mb.innerHTML.replace("more", n_siblings + " more");
+	
+	// add listener for toggle
 	mb.addEventListener("click", function() {
 		this.classList.toggle("show-more-active");
 		let siblings = getSiblings(this);
-		for (const s of siblings) {
-			if (s.classList.contains("event-more")) {
-			console.log(s);
-				s.classList.toggle("event-more-active");
+		if (this.classList.contains("show-more-active")) {
+			mb.innerHTML = mb.innerHTML.replace("Show", "Hide").replace(" more", "");
+			for (const s of siblings) {
+				s.classList.add("event-more-active");
+			}
+		}
+		else {
+			mb.innerHTML = mb.innerHTML.replace("Hide", "Show") + " more ";
+			for (const s of siblings) {
+				s.classList.remove("event-more-active");
 			}
 		}
 	});
