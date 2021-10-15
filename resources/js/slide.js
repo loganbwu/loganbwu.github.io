35614191f02ce529e07b8e330c39628770ee2abb
Logan Wu <loganbwu@gmail.com>
2021-10-15 16:24:23 +1100

    Fix animations on window resize

--- resources/js/slide.js
+++ resources/js/slide.js
@@ -1,43 +1,48 @@
 (function() {
   var elements;
   var windowHeight;
-
+	
   function init() {
     elements = document.querySelectorAll('section, h1');
     windowHeight = window.innerHeight;
+		
+		for (var i = 0; i < elements.length; i++) {
+			var element = elements[i];
+			var positionFromTop = elements[i].getBoundingClientRect().top;
+			
+			if (positionFromTop - windowHeight <= 0) {
+				element.classList.add('already-visible');
+			}
+			if (element.tagName == 'H1') {
+				element.classList.add('reveal-init');
+			}
+		}
+	}
 	
-	for (var i = 0; i < elements.length; i++) {
-		var element = elements[i];
-		var positionFromTop = elements[i].getBoundingClientRect().top;
+	function onResize() {
+    windowHeight = window.innerHeight;
+		checkPosition();
+	}
 	
-		if (positionFromTop - windowHeight <= 0) {
-			element.classList.add('already-visible');
+	function checkPosition() {
+		for (var i = 0; i < elements.length; i++) {
+			var element = elements[i];
+			var positionFromTop = elements[i].getBoundingClientRect().top;
+			
+			if (positionFromTop - windowHeight <= 0) {
+				if (element.tagName == 'SECTION') {
+					element.classList.add('come-in');
+				}
+				else if (element.tagName == 'H1') {
+					element.classList.add('reveal');
+				}
 		}
-		if (element.tagName == 'H1') {
-			element.classList.add('reveal-init');
-		}
-    }
-  }
+	}
+}
 
-  function checkPosition() {
-    for (var i = 0; i < elements.length; i++) {
-      var element = elements[i];
-      var positionFromTop = elements[i].getBoundingClientRect().top;
+window.addEventListener('scroll', checkPosition);
+window.addEventListener('resize', onResize);
 
-      if (positionFromTop - windowHeight <= 0) {
-		if (element.tagName == 'SECTION') {
-			element.classList.add('come-in');
-		}
-		else if (element.tagName == 'H1') {
-			element.classList.add('reveal');
-		}
-      }
-    }
-  }
-
-  window.addEventListener('scroll', checkPosition);
-  window.addEventListener('resize', checkPosition);
-
-  init();
-  checkPosition();
+init();
+checkPosition();
 })();
