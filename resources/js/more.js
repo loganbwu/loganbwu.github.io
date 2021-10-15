06581e7e1a7fa126cbeca5bda22c0218b54f6a47
Logan Wu <loganbwu@gmail.com>
2021-10-15 17:26:53 +1100

    Make more text buttons even more dynamic

--- resources/js/more.js
+++ resources/js/more.js
@@ -3,24 +3,28 @@ let getSiblings = function (e) {
     let siblings = [];
     
     // collecting siblings
-	let sibling = e.previousSibling;
+	let sibling = e.parentNode.firstChild;
     while (sibling) {
         if (sibling.nodeType === 1 && sibling.classList.contains("event-more")) {
             siblings.push(sibling);
         }
-        sibling = sibling.previousSibling;
+        sibling = sibling.nextSibling;
     }
     return siblings;
 };
 
-// Create 'more' text if .event-more appears
-// .event-more must be the last child of its parent
-var event_more = document.querySelectorAll(".event-more:last-child");
-var more_HTML = "<div class='show-more'>Show more </div>"
+// Create 'more' text at end of parent node if .event-more appears
+var event_more = document.querySelectorAll(".event-more:last-child"),
+	event_more_parent = [],
+	more_HTML = "<div class='show-more'>Show more </div>"
 for (const em of event_more) {
-	em.insertAdjacentHTML("afterend", more_HTML);
+	if (!event_more_parent.includes(em.parentNode)) {
+		event_more_parent.push(em.parentNode);	// remember not to use again
+		em.parentNode.insertAdjacentHTML("beforeend", more_HTML);
+	}
 }
 
+// Find the more text/buttons we've created
 var morebuttons = document.getElementsByClassName("show-more");
 for (const mb of morebuttons) {
 	// set text dynamically
