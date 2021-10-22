57dd658c528e7098387034b08bb27bf2915d408c
Logan Wu <loganbwu@gmail.com>
2021-10-22 11:46:14 +1100

    Fix bug in boids

--- resources/js/boids.js
+++ resources/js/boids.js
@@ -1,17 +1,17 @@
 const v = 4;
 const damping = 0.05;
 
-const max_dist_sep = 20;
-const max_dist_coh = 200;
+const max_dist_sep = 30;
+const max_dist_coh = 100;
 const wall_range = 100;
 
-const strength_sep = 10;
+const strength_sep = 5;
 const strength_coh = 3;
 const strength_ali = 0.5;
 
 const strength_wall = 0.001;
 const strength_noise = 0.01;
-const strength_mouse = 3;
+const strength_mouse = 4;
 
 // Create SVG boids
 var svg = document.getElementById("svg-content");
@@ -52,7 +52,8 @@ function animate() {
         // Calculate acceleration
          let acc_sep = [0, 0];
          let acc_coh = [0, 0];
-         let vel_ali = [0, 0];
+         let acc_ali = [0, 0];
+         let n_ali = 0;
         for (let j = 0; j < boids.length; j++) {
             if (j === i) { continue; }
             let dist_sep = [pos[i][0] - pos[j][0], pos[i][1] - pos[j][1]];
@@ -60,26 +61,23 @@ function animate() {
             let abs_dist_sep = norm(dist_sep);
             // Separation
             if (abs_dist_sep < max_dist_sep) {
-                acc_sep[0] = dist_sep_norm[0] * ((max_dist_sep - abs_dist_sep)/max_dist_sep)**2;
-                acc_sep[1] = dist_sep_norm[1] * ((max_dist_sep - abs_dist_sep)/max_dist_sep)**2;
+                acc_sep[0] += dist_sep_norm[0] * ((max_dist_sep - abs_dist_sep)/max_dist_sep)**2;
+                acc_sep[1] += dist_sep_norm[1] * ((max_dist_sep - abs_dist_sep)/max_dist_sep)**2;
             }
             if (abs_dist_sep < max_dist_coh) {
                 // Cohesion
                 acc_coh[0] -= dist_sep[0];
                 acc_coh[1] -= dist_sep[1];
                 // Alignment
-                vel_ali[0] += vel[j][0];
-                vel_ali[1] += vel[j][1];
+                acc_ali[0] += vel[j][0] - vel[i][0];
+                acc_ali[1] += vel[j][1] - vel[i][0];
+                n_ali += 1;
             }
         }
         // Normalise vectors
         acc_sep = normalise(acc_sep);
         acc_coh = normalise(acc_coh);
-
-        // Calculate neighborhood mean velocity
-        vel_ali = [vel_ali[0] / (boids.length-1), vel_ali[1] / (boids.length-1)];
-        // Calculate difference between neighborhood mean and boid's self velocity;
-        let acc_ali = [vel_ali[0] - vel[i][0], vel_ali[1] - vel[i][1]];
+        acc_ali = n_ali > 0 ? [acc_ali[0] / n_ali, acc_ali[1] / n_ali] : [0, 0];
         
         // Avoid walls
         let acc_wall = [0, 0];
